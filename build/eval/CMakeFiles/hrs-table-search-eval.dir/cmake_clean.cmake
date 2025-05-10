6e1bc61b4a144abbdd6a651a5bf14645393296d5
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 18:15:56 -0400

    Create cmake_clean.cmake

---
+++ build/eval/CMakeFiles/hrs-table-search-eval.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hrs-table-search-eval.dir/hrs-table-search-eval.cc.o"
+  "CMakeFiles/hrs-table-search-eval.dir/hrs-table-search-eval.cc.o.d"
+  "hrs-table-search-eval"
+  "hrs-table-search-eval.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hrs-table-search-eval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
