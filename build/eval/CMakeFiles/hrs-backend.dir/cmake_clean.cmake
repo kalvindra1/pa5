3964a748c415d2ef1870d64a6ed02222d04ed06a
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 17:42:58 -0400

    Create cmake_clean.cmake

---
+++ build/eval/CMakeFiles/hrs-backend.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hrs-backend.dir/hrs-backend.cc.o"
+  "CMakeFiles/hrs-backend.dir/hrs-backend.cc.o.d"
+  "hrs-backend"
+  "hrs-backend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hrs-backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
