9fb69d537934069f1503136c8e47b12f47b31db1
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 17:31:07 -0400

    Create CTestTestfile.cmake

---
+++ build/eval/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kp579/ece2400/pair-32/pa5-sys/eval
+# Build directory: /home/kp579/ece2400/pair-32/pa5-sys/build/eval
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
