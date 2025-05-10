643bef95146231171edb73a69c321ee7069aae5d
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 17:50:54 -0400

    Create cmake_clean.cmake

---
+++ build/eval/CMakeFiles/hrs-binary-search-eval.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hrs-linear-search-eval.dir/hrs-linear-search-eval.cc.o"
+  "CMakeFiles/hrs-linear-search-eval.dir/hrs-linear-search-eval.cc.o.d"
+  "hrs-linear-search-eval"
+  "hrs-linear-search-eval.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hrs-linear-search-eval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
