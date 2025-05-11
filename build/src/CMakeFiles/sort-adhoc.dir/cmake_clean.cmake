41fcfce0d1be01a644da31185c798fdee915ed9e
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 20:35:05 -0400

    Create cmake_clean.cmake

---
+++ build/src/CMakeFiles/sort-adhoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sort-adhoc.dir/sort-adhoc.cc.o"
+  "CMakeFiles/sort-adhoc.dir/sort-adhoc.cc.o.d"
+  "sort-adhoc"
+  "sort-adhoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sort-adhoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
