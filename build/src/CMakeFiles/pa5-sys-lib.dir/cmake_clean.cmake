300684f67284db03c3e8c75d262b4088b1a3b9d2
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 20:08:05 -0400

    Create cmake_clean.cmake

---
+++ build/src/CMakeFiles/pa5-sys-lib.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pa5-sys-lib.dir/HRSBinarySearch.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/HRSBinarySearch.cc.o.d"
+  "CMakeFiles/pa5-sys-lib.dir/HRSLinearSearch.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/HRSLinearSearch.cc.o.d"
+  "CMakeFiles/pa5-sys-lib.dir/HRSTableSearch.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/HRSTableSearch.cc.o.d"
+  "CMakeFiles/pa5-sys-lib.dir/HRSTreeSearch.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/HRSTreeSearch.cc.o.d"
+  "CMakeFiles/pa5-sys-lib.dir/Image.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/Image.cc.o.d"
+  "CMakeFiles/pa5-sys-lib.dir/ece2400-stdlib.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/ece2400-stdlib.cc.o.d"
+  "CMakeFiles/pa5-sys-lib.dir/mnist-utils.cc.o"
+  "CMakeFiles/pa5-sys-lib.dir/mnist-utils.cc.o.d"
+  "libpa5-sys-lib.a"
+  "libpa5-sys-lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pa5-sys-lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
