067176a32671483cf00bad8c67fe154cccfec862
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 20:48:13 -0400

    Create cmake_clean.cmake

---
+++ build/src/CMakeFiles/vector-adhoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector-adhoc.dir/vector-adhoc.cc.o"
+  "CMakeFiles/vector-adhoc.dir/vector-adhoc.cc.o.d"
+  "vector-adhoc"
+  "vector-adhoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector-adhoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
