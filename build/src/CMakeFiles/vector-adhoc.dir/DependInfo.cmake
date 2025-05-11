8cacc0d7fe302d3136af96c21a29474a26a1875b
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 20:47:47 -0400

    Create DependInfo.cmake

---
+++ build/src/CMakeFiles/vector-adhoc.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kp579/ece2400/pair-32/pa5-sys/src/vector-adhoc.cc" "src/CMakeFiles/vector-adhoc.dir/vector-adhoc.cc.o" "gcc" "src/CMakeFiles/vector-adhoc.dir/vector-adhoc.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
