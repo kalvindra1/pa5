2ad1ade6aba80073c3da7ca25197ac93c29bd444
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 20:43:28 -0400

    Create cmake_clean.cmake

---
+++ build/src/CMakeFiles/tree-adhoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tree-adhoc.dir/tree-adhoc.cc.o"
+  "CMakeFiles/tree-adhoc.dir/tree-adhoc.cc.o.d"
+  "tree-adhoc"
+  "tree-adhoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tree-adhoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
