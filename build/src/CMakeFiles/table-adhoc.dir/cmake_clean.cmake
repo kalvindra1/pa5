fe413daf5c39f399e7acc79fd6fece07c4dd08bb
kalvindra1 <kalvindraparasram@gmail.com>
2025-05-10 20:39:14 -0400

    Create cmake_clean.cmake

---
+++ build/src/CMakeFiles/table-adhoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/table-adhoc.dir/table-adhoc.cc.o"
+  "CMakeFiles/table-adhoc.dir/table-adhoc.cc.o.d"
+  "table-adhoc"
+  "table-adhoc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/table-adhoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
